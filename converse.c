7e9db325115f9eb235bd349087f4d55d8ddd49dc
Yousef Amar <yousefamar@gmail.com>
2014-11-08 12:28:33 +0000

    Add terminal resize signal listener

--- converse.c
+++ converse.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <ncurses.h>
+#include <signal.h>
 
 void render() {
-	printw("Hello World !!!");
+	//printw("Hello World !!!");
+	mvprintw(0, 0, "COLS = %d, LINES = %d", COLS, LINES);
 
 	refresh();
 }
 
+void on_winch (int sig) {
+	int i;
+
+	endwin();
+	refresh();
+	clear();
+
+	render();
+}
+
 int main (int argc, char *argv[]) {
+	struct sigaction sa;
+
 	initscr();
+
+	memset(&sa, 0, sizeof(struct sigaction));
+	sa.sa_handler = on_winch;
+	sigaction(SIGWINCH, &sa, NULL);
 
 	render();
 
-	getch();
+	while (getch() != 27);
+
 	endwin();
 
 	return 0;
