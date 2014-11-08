74ab5312aff9eef2993b385bcec76183add0b9b6
Yousef Amar <yousefamar@gmail.com>
2014-11-08 20:02:55 +0000

    Create very simple console-like interface
    The interface is a one line input area separated by a horizontal rule
    where every line entered (complete by pressing enter) is printed to the
    text area above line by line.

--- converse.c
+++ converse.c
@@ -1,29 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ncurses.h>
 #include <signal.h>
 
+#define INPUT_BUFF_LEN 65536
+#define LOG_LEN        1024
+
+typedef struct _MsgNode {
+	int length;
+	char *message;
+	struct _MsgNode *previous;
+} MsgNode;
+
+MsgNode *latest = NULL;
+char input[INPUT_BUFF_LEN];
+
+MsgNode* pushMessage(char *message) {
+	MsgNode *node = (MsgNode*) malloc(sizeof(MsgNode));
+
+	node->length = strlen(message + 1);
+	node->message = (char*) malloc(node->length * (sizeof(char)));
+	strcpy(node->message, message);
+	node->previous = latest;
+
+	return node;
+}
+
 void render() {
-	//printw("Hello World !!!");
-	mvprintw(0, 0, "COLS = %d, LINES = %d", COLS, LINES);
+	int i;
+	MsgNode *current = latest;
+
+	clear();
+
+	for (i = 0; i < COLS; i++)
+		mvaddch(LINES - 2, i, '_');
+
+	if (latest != NULL) {
+		for (i = 0; current != NULL; ++i, current = current->previous) {
+			mvprintw(LINES - 3 - i, 0, current->message);
+		}
+	}
+
+	mvprintw(LINES -1, 0, input);
 
 	refresh();
 }
 
 void on_winch (int sig) {
-	int i;
-
 	endwin();
 	refresh();
-	clear();
+
+	noecho();
+	raw();
+	keypad(stdscr, TRUE);
 
 	render();
 }
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
+	int i, j, ch;
 	struct sigaction sa;
 
+
 	initscr();
+	noecho();
+	raw();
+	keypad(stdscr, TRUE);
 
 	memset(&sa, 0, sizeof(struct sigaction));
 	sa.sa_handler = on_winch;
@@ -31,9 +74,32 @@ int main (int argc, char *argv[]) {
 
 	render();
 
-	while (getch() != 27);
+	i = 0;
+	while ((ch = wgetch(stdscr)) != 27) {
+		if (ch > 31 && ch < 127) {
+			input[i] = ch;
+			i++;
+		} else {
+			switch (ch) {
+				case KEY_ENTER:
+				case '\n':
+				case '\r':
+					input[i] = '\0';
+					latest = pushMessage(input);
+					for (j = 0; j < i; j++)
+						input[j] = '\0';
+					i = 0;
+					break;
+				default:
+					break;
+			}
+		}
+		render();
+	}
 
 	endwin();
+
+	// TODO: Free all memory.
 
 	return 0;
 }
