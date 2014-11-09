9024d8941d8c3ce110871f713f18366c58a64f60
Yousef Amar <yousefamar@gmail.com>
2014-11-09 15:11:10 +0000

    Implement rudimentary text wrapping and input scrolling

--- converse.c
+++ converse.c
@@ -5,6 +5,7 @@
 #include <signal.h>
 
 #define INPUT_BUFF_LEN 65536
+// TODO: Actually implement this
 #define LOG_LEN        1024
 
 typedef struct _MsgNode {
@@ -15,11 +16,12 @@ typedef struct _MsgNode {
 
 MsgNode *latest = NULL;
 char input[INPUT_BUFF_LEN];
+int input_len = 0;
 
 MsgNode* pushMessage(char *message) {
 	MsgNode *node = (MsgNode*) malloc(sizeof(MsgNode));
 
-	node->length = strlen(message + 1);
+	node->length = strlen(message);
 	node->message = (char*) malloc(node->length * (sizeof(char)));
 	strcpy(node->message, message);
 	node->previous = latest;
@@ -28,21 +30,31 @@ MsgNode* pushMessage(char *message) {
 }
 
 void render() {
-	int i;
+	int i, id, col, line, lines;
 	MsgNode *current = latest;
 
 	clear();
 
-	for (i = 0; i < COLS; i++)
-		mvaddch(LINES - 2, i, '_');
-
 	if (latest != NULL) {
-		for (i = 0; current != NULL; ++i, current = current->previous) {
-			mvprintw(LINES - 3 - i, 0, current->message);
+		for (i = 0; i < LINES - 2 && current != NULL; current = current->previous) {
+			// TODO: Implement text wrap
+			lines = current->length / COLS + 1;
+			i += lines;
+			id = 0;
+			for (line = 0; line < lines; ++line)
+				for (col = 0; id < current->length && col < COLS; ++col, ++id)
+					if (LINES - 2 - i + line >= 0)
+						mvaddch(LINES - 2 - i + line, col, current->message[id]);
 		}
 	}
 
-	mvprintw(LINES -1, 0, input);
+	for (i = 0; i < COLS; i++)
+		mvaddch(LINES - 2, i, '_');
+
+	id = input_len - COLS;
+	if (id < 0) id = 0;
+	for (i = 0; i < input_len && i < COLS; ++i)
+		mvaddch(LINES - 1, i, input[id++]);
 
 	refresh();
 }
@@ -62,6 +74,9 @@ int main(int argc, char *argv[]) {
 	int i, j, ch;
 	struct sigaction sa;
 
+	// Init input buffer to all null chars for faster wrap checks
+	for (i = 0; i < INPUT_BUFF_LEN; ++i)
+		input[i] = '\0';
 
 	initscr();
 	noecho();
@@ -74,21 +89,20 @@ int main(int argc, char *argv[]) {
 
 	render();
 
-	i = 0;
+	input_len = 0;
 	while ((ch = wgetch(stdscr)) != 27) {
 		if (ch > 31 && ch < 127) {
-			input[i] = ch;
-			i++;
+			input[input_len] = ch;
+			input_len++;
 		} else {
 			switch (ch) {
 				case KEY_ENTER:
 				case '\n':
 				case '\r':
-					input[i] = '\0';
 					latest = pushMessage(input);
-					for (j = 0; j < i; j++)
+					for (j = 0; j < input_len; j++)
 						input[j] = '\0';
-					i = 0;
+					input_len = 0;
 					break;
 				default:
 					break;
@@ -99,7 +113,7 @@ int main(int argc, char *argv[]) {
 
 	endwin();
 
-	// TODO: Free all memory.
+	// TODO: Free all memory
 
 	return 0;
 }
