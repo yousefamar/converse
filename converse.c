44b591c0b8340d1567a9b18527d2dbc07aca2daa
Yousef Amar <yousefamar@gmail.com>
2014-11-23 11:11:36 +0000

    Document code

--- converse.c
+++ converse.c
@@ -6,21 +6,35 @@
 #include <pthread.h>
 #include <unistd.h>
 
+/** The maximum length (in characters) of any message */
 #define LINE_BUFF_LEN 65536
+/** How many logged messages are stored in memory */
 // TODO: Actually implement this
 #define LOG_LEN       1024
 #define NEWLINE       '\n'
 
+/**
+ * A stack that stores logged messages.
+ */
 typedef struct _MsgNode {
 	int length;
 	char *message;
 	struct _MsgNode *previous;
 } MsgNode;
 
+/** The top node on the stack (latest logged message) */
 MsgNode *latest = NULL;
+/** The user input buffer */
 char input[LINE_BUFF_LEN];
 int input_len = 0;
 
+/**
+ * Adds a message to the bottom of the message log.
+ *
+ * @param message The message to be logged.
+ *
+ * @return node The message node containing the latest logged message.
+ */
 // TODO: Make threadsafe; use mutex
 MsgNode* pushMessage(char *message) {
 	MsgNode *node = (MsgNode*) malloc(sizeof(MsgNode));
@@ -33,15 +47,19 @@ MsgNode* pushMessage(char *message) {
 	return node;
 }
 
+/**
+ * Redraws the ncurses UI.
+ */
 void render() {
 	int i, id, col, line, lines;
 	MsgNode *current = latest;
 
 	clear();
 
+	/* Render message log from latest to top of window with rudimentary soft wrapping */
 	if (latest != NULL) {
 		for (i = 0; i < LINES - 2 && current != NULL; current = current->previous) {
-			// TODO: Implement text wrap
+			// TODO: Implement word soft wrapping
 			lines = current->length / COLS + 1;
 			i += lines;
 			id = 0;
@@ -52,17 +70,27 @@ void render() {
 		}
 	}
 
+	/* Render horizontal rule above user input area */
 	for (i = 0; i < COLS; i++)
 		mvaddch(LINES - 2, i, '_');
 
+	/* Render input buffer making sure the rightmost characters are always visible
+	 * (if the text is longer than the terminal window allows) */
 	id = input_len - COLS;
 	if (id < 0) id = 0;
 	for (i = 0; i < input_len && i < COLS; ++i)
 		mvaddch(LINES - 1, i, input[id++]);
 
+	/* ncurses redraw */
 	refresh();
 }
 
+/**
+ * A thread start routine that watches a file
+ * and writes any new lines to the message log.
+ *
+ * @param arg The path of the file to be watched.
+ */
 void* watchFile(void* arg) {
 	char *path = (char*) arg;
 	int i = 0;
@@ -79,15 +107,21 @@ void* watchFile(void* arg) {
 		exit(1);
 	}
 
+	/* Read characters from the file forever
+	 * NOTE: This is to allow for any new data to come in even after the end of
+	 *       the file (EOF) which is especially necessary for FIFO files. */
 	while (1) {
 		c = fgetc(file);
 
+		/* If the end of the file is reached, reduce character polling frequency */
 		if(c == EOF) {
 			// TODO: Perhaps count the number of EOFs and scale sleep time relatively.
+			/* Pause thread for 100ms */
 			usleep(100000);
 			continue;
 		}
 
+		/* If a newline character is reached, log the line and reset the line buffer */
 		if (c == NEWLINE) {
 			line_buff[i] = '\0';
 			latest = pushMessage(line_buff);
@@ -96,6 +130,7 @@ void* watchFile(void* arg) {
 			continue;
 		}
 
+		/* Add any other polled character to the line buffer */
 		if (i < (LINE_BUFF_LEN - 1))
 			line_buff[i] = c;
 
@@ -110,6 +145,9 @@ void* watchFile(void* arg) {
 	return NULL;
 }
 
+/**
+ * Handles window resize signal.
+ */
 void onWinch (int sig) {
 	endwin();
 	refresh();
@@ -118,15 +156,23 @@ void onWinch (int sig) {
 	raw();
 	keypad(stdscr, TRUE);
 
+	/* Redraw UI */
 	render();
 }
 
+/**
+ * converse entry point.
+ *
+ * @arg input_file  The path to a file watched for input to the message log.
+ * @arg output_file The path to a file to which user input messages are appended.
+ */
 int main(int argc, char *argv[]) {
 	int i, j, ch;
 	struct sigaction sa;
 	pthread_t thread;
 	FILE *file_out;
 
+	/* Print usage and quit if incorrect argument count */
 	if (argc != 3) {
 		printf("usage: %s input_file output_file\n", argv[0]);
 		return 0;
@@ -139,25 +185,31 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	// Init input buffer to all null chars for faster wrap checks
+	/* Init input buffer to all null chars for faster wrap checks */
 	for (i = 0; i < LINE_BUFF_LEN; ++i)
 		input[i] = '\0';
 
+	/* Init ncurses */
 	initscr();
 	noecho();
 	raw();
 	keypad(stdscr, TRUE);
 
+	/* Register window resize callback */
 	memset(&sa, 0, sizeof(struct sigaction));
 	sa.sa_handler = onWinch;
 	sigaction(SIGWINCH, &sa, NULL);
 
 	render();
 
+	/* Run input_file watch thread */
 	pthread_create(&thread, NULL, watchFile, argv[1]);
 
+	/* Read and handle user keyboard input */
 	input_len = 0;
+	/* Quit on Esc */
 	while ((ch = wgetch(stdscr)) != 27) {
+		/* Add alphanumeric characters to input buffer */
 		if (ch > 31 && ch < 127) {
 			if (input_len < (LINE_BUFF_LEN - 1)) {
 				input[input_len++] = ch;
@@ -166,11 +218,14 @@ int main(int argc, char *argv[]) {
 			}
 		} else {
 			switch (ch) {
+				/* Delete last character in input buffer on backspace */
 				case KEY_BACKSPACE:
 				case 127:
 					if (input_len > 0)
 						input[--input_len] = '\0';
 					break;
+				/* Send message to output_file, push message to log,
+				 * and clear input buffer on Enter */
 				case KEY_ENTER:
 				case '\n':
 				case '\r':
@@ -190,6 +245,7 @@ int main(int argc, char *argv[]) {
 
 	endwin();
 
+	/* Clean up and destroy resources */
 	fclose(file_out);
 	// TODO: Free all memory, destroy threads, and free mutexes
 
