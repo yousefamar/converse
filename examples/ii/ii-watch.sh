25106d47c55967ab4e50d1cba73c0d8403c3f793
Yousef Amar <yousefamar@gmail.com>
2016-01-09 14:39:39 +0100

    Launch converse through ii-watch script on joining ii channel

--- examples/ii/ii-watch.sh
+++ examples/ii/ii-watch.sh
@@ -7,7 +7,7 @@ if [ $# -lt 2 ]; then
 	echo "  II_DIR:  Path to ii root directory"
 	echo "  PATTERN: Regex that when matched, triggers a notification"
 	echo "    Case insensitive and excludes timestamps and sender usernames"
-	echo "    e.g. '.' for everything, '$(whoami)' for username mentions, etc"
+	echo "    e.g. '.' for everything, '\<$(whoami)\>' for username mentions, etc"
 	exit
 fi
 
@@ -25,7 +25,13 @@ while true; do
 	directory=$(echo "$notification" | cut -f1 -d ' ')
 	event=$(echo "$notification" | cut -f2 -d ' ')
 	file=$(echo "$notification" | cut -f3 -d ' ')
-	[ "$event" = "CREATE,ISDIR" ] && notify-send -u critical -- "Channel Joined" "$directory$file"
+	if [ "$event" = "CREATE,ISDIR" ]; then
+		notify-send -u critical -- "Channel Joined" "$directory$file"
+		# Wait until ii files are created
+		# FIXME: This will lock up if a non-ii directory is added for whatever reason
+		while [ ! -e "$directory$file/in" -o ! -e "$directory$file/out" ]; do inotifywait -qqt 1 -e create "$directory$file"; done
+		$(dirname $(readlink -f "$0"))/converse-ii.sh "$directory$file" &
+	fi
 	if [ "$file" = "out" ]; then
 		# TODO: Add special case for differentiating networks on IRC bouncers
 		server=$(echo "$directory" | awk -F/ '{print $(NF-2)}')
